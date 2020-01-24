b982ebac8694cd43c0b8f3a316f05e2f04a2bd98
to4my <to4my@ya.ru>
2020-01-24 15:37:22 +0300

    added show.html.erb_spec.rb

---
+++ spec/views/user/show.html.erb_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe 'users/show', type: :view do
+  let(:user) { FactoryGirl.build_stubbed(:user, name: 'Mike') }
+
+  it 'shows username' do
+    assign(:user, user)
+
+    render
+
+    expect(rendered).to match 'Mike'
+  end
+
+  it 'shows change password button if current_user' do
+    assign(:user, user)
+    allow(view).to receive(:current_user).and_return(user)
+
+    render
+
+    expect(rendered).to match 'Сменить имя и пароль'
+  end
+
+  it 'does not show change password button if not current_user' do
+    another_user = FactoryGirl.build_stubbed(:user, name: 'Mike')
+
+    assign(:user, another_user)
+    allow(view).to receive(:current_user).and_return(user)
+
+    render
+
+    expect(rendered).not_to match 'Сменить имя и пароль'
+  end
+
+  it 'shows game partial' do
+    assign(:user, user)
+    assign(:games, [FactoryGirl.build_stubbed(:game)])
+
+    stub_template 'users/_game.html.erb' => 'User game goes here'
+
+    render
+
+    expect(rendered).to match 'User game goes here'
+  end
+end
